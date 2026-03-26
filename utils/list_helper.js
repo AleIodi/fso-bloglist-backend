d910f0d626dfdb875bb2271b00a58513edac19d5
Your Name <you@example.com>
2026-03-26 17:29:57 +0100

    introduction of testing 2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,7 +8,22 @@ const totalLikes = (blogs) => {
 		: blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog  = (blogs) => {
+	if (blogs.length === 0)
+		return null;
+	const favourite = blogs.reduce((prev, current) => {
+		return prev.likes > current.likes ? prev : current
+	})
+
+	return {
+		title: favourite.title,
+		author: favourite.author,
+		url: favourite.url,
+		likes: favourite.likes
+	}
+}
 module.exports = {
 	dummy,
-	totalLikes
+	totalLikes,
+	favoriteBlog
 }

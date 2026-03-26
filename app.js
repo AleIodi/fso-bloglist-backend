94c8fb9f437948e615c33a8abc65e886a72131e5
Your Name <you@example.com>
2026-03-26 16:12:52 +0100

    refactor backend

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
+const blogRouter = require('./controllers/blogs')
+
+const app = express()
+
+morgan.token('body', (req, res) => {
+	return JSON.stringify(req.body)
+})
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl, { family: 4 })
+.then(() => console.log('Connesso a MongoDB'))
+.catch(err => console.error('Errore di connessione:', err))
+
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
